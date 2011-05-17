caeee07e1ef61942de6df3352f98503e711361b0
Joel Rivera <joelriv@gmail.com>
2011-05-17 11:00:20 -0500

    Adding a little bit more of description in the setup.py to reflect the latest change in the version

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup
 
 setup(name='awake',
-      version='0.6',
+      version='0.6.1',
       author='Joel Rivera',
       author_email='joelriv@gmail.com',
+      url='http://github.com/cyraxjoe/awake',
       license='GPL',
       package_dir={'':'lib/'},
       scripts=['awake.py',],
       py_modules=['wol',],
+      provides=['wol',],
       classifiers=['Environment :: Console',
-                   'Operating System :: OS Independent'],
+                   'Operating System :: OS Independent',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Programming Language :: Python :: 2.4'],
       description='Command line program to wake-on-lan a remote host.'
       )
