93ee021b1d5146ee285ea90270df0ab3c07001e3
Joel Rivera <joelriv@gmail.com>
2012-04-27 01:33:57 -0500

    This is the first commit of the new release, which is going to break the internal api, add support for python3, ipv6, different kinds of macformats, and ipformats also some tests.

---
+++ tests/test_awakecli.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class TestCli(unittest.TestCase):
+
+    def test_invalid_argument(self):
+        pass
+
+    def test_missin_required_argument(self):
+        pass
+
+    def test_1000_macs_in_args(self):
+        pass
+
+    def test_bad_mac_in_the_middle_of_args(self):
+        pass
+    
+    def test_verbose_option(self):
+        pass
+
+    def test_help_option(self):
+        pass
+
+    def test_dest_option(self):
+        pass
+
+    def test_port_option(self):
+        pass
+
+    def test_file_option(self):
+        pass
+
+    def test_file_option_and_args(self):
+        pass
+
+    def test_separation_option(self):
+        pass
+    
+
+    def test_multiple_file_options(self):
+        pass
+
+
+
+
+class TestFileOfMacsInCLI(unittest.TestCase):
+    def test_big_file(self):
+        pass
+
+
+    def test_not_exists_file(self):
+        pass
+
+
+    def test_unable_to_read_file(self):
+        pass
+
+    
+    def test_bad_macs_in_file(self):
+        pass
+    
+        
+
+
+
+    
+
+        
+
+        
+
+
---
+++ tests/test_awakelib.py
@@ -0,0 +1,31 @@
+import unittest
+
+class TestMACFormat(unittest.TestCase):
+
+    def test_invalid_hexnumber(self):
+        pass
+
+    def test_too_long(self):
+        pass
+
+    def test_too_short(self):
+        pass
+
+    def test_junk_bytes(self):
+        pass
+
+    def test_lowercase(self):
+        pass
+
+    def test_uppercase(self):
+        pass
+
+    def test_no_separator(self):
+        pass
+
+    def test_ascii_chars_as_separator(self):
+        pass
+
+    def test_non_ascii_chars_as_separator(self):
+        uchars = ['ñ', 'ಠ', '¡', '¿']
+        
